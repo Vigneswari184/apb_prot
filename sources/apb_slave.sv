e2e0c7d44316710c515b1dacd2663bc8698d6c86
Vigneswari184 <Engineers@Rvega.co.in>
2026-03-11 17:30:52 +0530

    Create apb_slave.sv

---
+++ sources/apb_slave.sv
@@ -0,0 +1,20 @@
+module apb_slave #(
+    parameter ADDR_WIDTH = 8,
+    parameter DATA_WIDTH = 32,
+    parameter REG_NUM = 4       // Number of registers
+)(
+    input  wire                  PCLK,
+    input  wire                  PRESET_n,
+    input  wire [ADDR_WIDTH-1:0] PADDR,
+    input  wire [DATA_WIDTH-1:0] PWDATA,
+    input  wire                  PWRITE,
+    input  wire                  PSELx,
+    input  wire                  PENABLE,
+    output reg  [DATA_WIDTH-1:0] PRDATA,
+    output reg                   PREADY,
+    output reg                   PSLVERR
+);
+
+//
+
+endmodule
